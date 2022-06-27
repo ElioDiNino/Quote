2641f94ad142cf556d26bad05956b2ef54b9d262
Elio Di Nino <52972357+ElioDiNino@users.noreply.github.com>
2022-06-27 15:35:19 -0700

    Discord Canary support

--- src/regexps.ts
+++ src/regexps.ts
@@ -1,2 +1,2 @@
 export const MARKDOWN = /^\>\s(?<text>.+)/;
-export const URL = /https:\/\/(ptb\.)?discord(app)?.com\/channels\/(?<guildId>[0-9]+)\/(?<channelId>[0-9]+)\/(?<messageId>[0-9]+)\/?/;
+export const URL = /https:\/\/((canary\.)|(ptb\.))?discord(app)?.com\/channels\/(?<guildId>[0-9]+)\/(?<channelId>[0-9]+)\/(?<messageId>[0-9]+)\/?/;
